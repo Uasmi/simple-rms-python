b00a8529552748c80d0a2c2a0fefc978208dc1f4
Uasmi <uasmi.nasser@gmail.com>
2019-07-19 14:04:15 +0300

    Removed unnecessary dependencies

--- rms.py
+++ rms.py
@@ -3,13 +3,10 @@ import numpy as np
 import wave
 import struct
 import time
-import PIL
-from PIL import Image
-import matplotlib.pyplot as plt
-import matplotlib
+
 import cv2
 import time 
-matplotlib.use('TkAgg')
+
 Chunk = 256
 
 p = pyaudio.PyAudio()
@@ -24,14 +21,8 @@ tmpArray = np.empty([], np.int8)
 print (tmpArray.shape)
 data = wf.readframes(Chunk)
 i = 0
-tmpList = []
-finalList = []
 
-#---Graphics---#
 
-fig, ax = plt.subplots()
-ax.plot(2,10)
-#--------------#
 img = np.zeros([200,100,1])
 
 
